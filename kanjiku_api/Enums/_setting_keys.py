69132c9972f9c81c16e88ada43987a7e84b36248
Aurvandill <cerberus885@gmail.com>
2024-05-21 23:20:08 +0200

    added permissions to groups

--- kanjiku_api/Enums/_setting_keys.py
+++ kanjiku_api/Enums/_setting_keys.py
@@ -2,5 +2,13 @@ from enum import Enum
 
 
 class SettingKeys(Enum):
+    """These are keys which are allowed for dynamic configuration entrys.
+    """
     Favicon = "Favicon"
+    """Config key for the favicon"""
     Logo = "Logo"
+    """Config key for the site logo"""
+    TeamMemberRole = "TeamMemberRole"
+    """This role will be the indicator for team members."""
+    SupporterRole = "SupporterRole"
+    """This role will be the indicator for supporters."""
--- kanjiku_api/data_models/_group.py
+++ kanjiku_api/data_models/_group.py
@@ -10,12 +10,24 @@ class Group(Model):
     Groups are used for Permission.
 
     Attributes:
-        name (str): Name of the Usergroup
-        users (list[User]): List of Users in this group
+        name (str): Name of the Usergroup.
+        users (list[User]): List of Users in this group.
+        early_access (int): How many days before Release someone can read Stuff.
+        upload_chapters (bool): Can Upload chapters and modify them.
+        moderate_avatars (bool): Can delete Avatars for all Users.
+        manage_projects (bool): Can create/update/delete Mangas.
+        view_hidden (bool): Can view hidden Chapters.
+        admin (bool): Full administrative access.
     """
 
     name: str = fields.CharField(unique=True, max_length=50, pk=True)
     users: fields.ManyToManyRelation[User]
+    early_access: int = fields.IntField(default=0)
+    upload_chapters = fields.BooleanField(default=False)
+    moderate_avatars = fields.BooleanField(default=False)
+    manage_projects = fields.BooleanField(default=False)
+    view_hidden = fields.BooleanField(default=False)
+    admin = fields.BooleanField(default=False)
 
     def __str__(self):
         return self.name

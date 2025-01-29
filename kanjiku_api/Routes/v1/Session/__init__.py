a76beab22d833c4df120e7bcadb30de7ecb5adac
Aurvandill <cerberus885@gmail.com>
2025-01-29 21:15:16 +0100

    basic info about the session

--- kanjiku_api/Routes/v1/Session/__init__.py
+++ kanjiku_api/Routes/v1/Session/__init__.py
@@ -1,5 +1,28 @@
-from sanic import Blueprint
+import i18n
+from sanic import Blueprint, Request
+from sanic.response import json as json_resp
+from kanjiku_api.Utility import JWTHelper
+from kanjiku_api.Exceptions import SessionError
+from kanjiku_api.data_models import IdentityToken
 
 session_bp = Blueprint("Session", "/Session")
 
 from . import _login, _refresh, _logout
+
+@session_bp.route("/", ["GET"])
+async def session_info(request: Request):
+    jwt_helper: JWTHelper = request.app.ctx.jwt
+
+    identity_token = request.cookies.get("IdentityToken", None)
+    if identity_token is None:
+        raise SessionError(
+            {
+                "msg": i18n.t("errors.no_refresh_token_provided"),
+                "msg_key": "errors.no_refresh_token_provided",
+            },
+            status_code=400,
+        )
+
+    user_data, token_id = jwt_helper.token_data(identity_token)
+
+    return json_resp(user_data)

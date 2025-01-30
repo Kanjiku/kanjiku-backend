d5bb426884158a958db6ff3e22e8847c8467fb55
Aurvandill <cerberus885@gmail.com>
2025-01-30 22:51:20 +0100

    example for pagination and stuff

--- example_requests/show_users_example.py
+++ example_requests/show_users_example.py
@@ -29,15 +29,31 @@ resp = requests.post(
         "birthday": "1997-07-13",
     },
 )
-print("#### Show all Users")
+print("#### Show 'all' Users (page 1 pagesize 25 by default)")
 resp = requests.get("http://localhost:9999/v1/User")
-
 resp_json = resp.json()
 print(json.dumps(resp_json, indent=4))
 
+print("#### Pagination example")
+print("Page 1 pagesize 2")
+resp = requests.get("http://localhost:9999/v1/User?page=1&pagesize=2")
+resp_json = resp.json()
+# small var for the user id of the first user we need later on
 first_user = list(resp_json["users"].keys())[0]
+print(json.dumps(resp_json, indent=4))
+
+print("Page 2 pagesize 2")
+resp = requests.get("http://localhost:9999/v1/User?page=2&pagesize=2")
+resp_json = resp.json()
+print(json.dumps(resp_json, indent=4))
+
+print("Page 99999 pagesize 2")
+resp = requests.get("http://localhost:9999/v1/User?page=99999&pagesize=2")
+resp_json = resp.json()
+print(json.dumps(resp_json, indent=4))
+
+
 print(f"\n#### show user with id {first_user}")
 resp = requests.get(f"http://localhost:9999/v1/User/{first_user}")
-print(resp.text)
 resp_json = resp.json()
 print(json.dumps(resp_json, indent=4))

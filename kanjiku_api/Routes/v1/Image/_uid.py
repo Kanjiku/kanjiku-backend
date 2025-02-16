0a61841384ce3217507e2cd3d3489af1ac45b158
Aurvandill <cerberus885@gmail.com>
2025-02-16 13:44:41 +0100

    check if the thumbnail image exists, if not return original image

--- kanjiku_api/Routes/v1/Image/_uid.py
+++ kanjiku_api/Routes/v1/Image/_uid.py
@@ -8,8 +8,10 @@ from kanjiku_api.Utility import ImageHandler
 from kanjiku_api.Exceptions import ImageDoesNotExist
 
 from . import image_bp
+
+
 @image_bp.route("/<image_id:uuid>", ["GET"])
-async def show_user_by_id(request: Request, image_id: UUID):
+async def get_image(request: Request, image_id: UUID):
     image = await Image.get_or_none(uuid=image_id)
     if image is None:
         raise ImageDoesNotExist(
@@ -23,3 +25,23 @@ async def show_user_by_id(request: Request, image_id: UUID):
     image_handler: ImageHandler = request.app.ctx.image_handler
 
     return await image_handler.get_file(image_id, image.filename)
+
+
+from . import image_bp
+
+
+@image_bp.route("/<image_id:uuid>/thumbnail", ["GET"])
+async def get_image_thumbnail(request: Request, image_id: UUID):
+    image = await Image.get_or_none(uuid=image_id)
+    if image is None:
+        raise ImageDoesNotExist()
+
+    image_handler: ImageHandler = request.app.ctx.image_handler
+    if image_handler.exists(f"{image_id}_thumbnail"):
+        img_resp = await image_handler.get_file(
+            f"{image_id}_thumbnail", f"{image.filename}_thumbnail.webp"
+        )
+    else:
+        img_resp = await image_handler.get_file(image_id, image.filename)
+
+    return img_resp

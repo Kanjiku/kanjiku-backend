66508980da217e1ccac6d2d6ef5898b7cd31c2df
Aurvandill <cerberus885@gmail.com>
2025-03-02 19:22:09 +0100

    define users

--- kanjiku_api/__main__.py
+++ kanjiku_api/__main__.py
@@ -1,12 +1,41 @@
+import bcrypt
 import tomllib
+import logging
 
 from sanic import Sanic
 from pathlib import Path
+from typing import Optional
 from functools import partial
 from sanic.worker.loader import AppLoader
 
 from kanjiku_api.Enums import SignMethod
 from kanjiku_api import create_app, cli
+from kanjiku_api.data_models import User
+
+logger = logging.getLogger("kanjiku_backend")
+
+
+async def define_users(_app, _loop, user_config: Optional[list[dict]] = None):
+    if user_config is None:
+        return
+    for user_data in user_config:
+
+        username = user_data["username"]
+        password = user_data["password"]
+        admin = user_data.get("admin", False)
+
+        user = await User.get_or_none(username=username)
+        if user is not None:
+            logger.info(f"{username} already exists skipping creation")
+            continue
+
+        logger.info(f"creating user {username}")
+
+        salt = bcrypt.gensalt()
+        pw_hash = bcrypt.hashpw(password.encode("utf-8"), salt)
+
+        user = await User.create(username=username, password_hash=pw_hash, admin=admin)
+        await user.save()
 
 
 if __name__ == "__main__":
@@ -15,6 +44,10 @@ if __name__ == "__main__":
     cfg["JWT"]["signmethod"] = SignMethod(cfg["JWT"].pop("algorithm"))
     loader = AppLoader(factory=partial(create_app, cfg))
     app = loader.load()
+
+    app.register_listener(
+        partial(define_users, user_config=cfg.get("users", None)), "main_process_start"
+    )
 
     app.prepare(
         host=cfg.get("listening_ip", "localhost"),
